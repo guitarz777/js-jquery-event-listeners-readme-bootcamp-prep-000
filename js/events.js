b2e08b0ddf8bec15e32f716a5a4c18e8d1d0cfa5
guitarz777@gmail.com <guitarz777@gmail.com>
2018-09-08 20:45:46 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -8,7 +8,7 @@ function getIt(){
 function frameIt(){
   $("img").on("load", function(e){
     $("img").addClass('tasty');
-    $("img").css('border', '2px solid red' )
+    $("img").css('background-color', 'green' )
   });
 }
 
